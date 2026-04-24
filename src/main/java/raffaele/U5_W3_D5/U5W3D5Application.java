36754b5c2cf3245bbb816344c0eedce5d8d61563
raffaelebaiocco06-ux <raffaele.baiocco06@gmail.com>
2026-04-24 09:16:49 +0200

    starter

---
+++ src/main/java/raffaele/U5_W3_D5/U5W3D5Application.java
@@ -0,0 +1,13 @@
+package raffaele.U5_W3_D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3D5Application.class, args);
+	}
+
+}
