7aaa00ce05d8e828dc9bcb5cf213b3874e3bba51
raffaelebaiocco06-ux <raffaele.baiocco06@gmail.com>
2026-04-24 10:54:22 +0200

    repository fatte

---
+++ src/main/java/raffaele/U5_W3_D5/repositories/PrenotazioneRepository.java
@@ -0,0 +1,20 @@
+package raffaele.U5_W3_D5.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import raffaele.U5_W3_D5.entities.Evento;
+import raffaele.U5_W3_D5.entities.Prenotazione;
+import raffaele.U5_W3_D5.entities.Utente;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PrenotazioneRepository extends JpaRepository<Prenotazione, Long> {
+
+    List<Prenotazione> findByUtenteId(Long utenteId);
+
+    List<Prenotazione> findByEvento(Evento evento);
+
+    boolean existsByUtenteAndEvento(Utente utente, Evento evento);
+
+    Optional<Prenotazione> findByUtenteAndEvento(Utente utente, Evento evento);
+}
